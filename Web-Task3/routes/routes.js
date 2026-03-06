f0ee84d7d4b0fa9bb315d6db746148da03795a63
Minahil Kashif <minahilkashif1117@gmail.com>
2026-03-06 16:00:37 +0500

    Implemented logout functionality

--- Web-Task3/routes/routes.js
+++ Web-Task3/routes/routes.js
@@ -40,7 +40,7 @@ router.get('/dashboard', authMiddleware, (req, res) => {
   res.send(`Welcome ${req.session.user}`);
 });
 
-// Logout Route
+// Logout Route added wowww
 router.get('/logout', (req, res) => {
   req.session.destroy(() => {
     res.send('Logout successful');
