277296f615cc245cbb34796a30ab363467c334da
Minahil Kashif <minahilkashif1117@gmail.com>
2026-03-06 15:56:43 +0500

    Added MongoDB connection using Mongoose

---
+++ Web-Task3/db.js
@@ -0,0 +1,11 @@
+// db.js
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/studentDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+module.exports = mongoose;
